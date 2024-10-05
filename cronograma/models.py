253022bea6876c0fbfcb617c06fd30caf87e66fe
Marcosespa <marcosespa24@gmail.com>
2024-10-05 10:56:04 -0500

    valor

--- cronograma/models.py
+++ cronograma/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Cronograma(models.Model):
     id_cronograma = models.IntegerField(default=0)
-    valor_total = models.DecimalField(max_digits=10, decimal_places=2)
+    valor_total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     mes = models.CharField(max_length=50)
 
     def __str__(self):
