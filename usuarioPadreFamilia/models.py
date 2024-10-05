a74bd47853a98f1ae5419bbe0e74c42554880ac1
Marcosespa <marcosespa24@gmail.com>
2024-10-05 11:00:44 -0500

    d

--- usuarioPadreFamilia/models.py
+++ usuarioPadreFamilia/models.py
@@ -7,7 +7,7 @@ class UsuarioPadreFamilia(models.Model):
     username = models.CharField(max_length=50)
     username = models.CharField(max_length=50)
     contrasenia = models.CharField(max_length=50)
-    descripcion = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=255, default='Sin descripción')
     estudiante_relacionado = models.CharField(max_length=100)
     def __str__(self):
         return self.username
