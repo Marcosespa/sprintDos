d3d2d62bee29ce2d6b22b639f2bb8ef722af6e4c
Marcosespa <marcosespa24@gmail.com>
2024-10-05 10:58:34 -0500

    d

--- descuento/models.py
+++ descuento/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Descuento(models.Model):
-    id_descuento = models.AutoField(primary_key=True)
-    fecha_aplicacion = models.DateField()
+    id_descuento = models.IntegerField(default=0)
+    fecha_aplicacion = models.DateField(auto_now_add=True)
     valor_descuento = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
