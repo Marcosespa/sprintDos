130a719f63dd1802573a5f5b3603de4d1ba05d5b
Marcosespa <marcosespa24@gmail.com>
2024-10-06 06:27:11 -0500

    d

--- sprintDos/settings.py
+++ sprintDos/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'sprintDos.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'sprintDos', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
