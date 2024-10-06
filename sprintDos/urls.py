6fb0a3f30b456608d1ab68d534f0cbc777a77ae0
Marcosespa <marcosespa24@gmail.com>
2024-10-06 16:04:52 -0500

    checkpoint

--- sprintDos/urls.py
+++ sprintDos/urls.py
@@ -5,8 +5,8 @@ import usuarioPadreFamilia.urls  # Asegúrate de importar las URLs de tu app
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),  # Página principal para el index
+    #path('', views.index, name='index'),  # Página principal para el index
+    path('', include('usuarioPadreFamilia.urls')),
     path('crear-usuario/', views.crear_usuario, name='crear_usuario'),
-    path('usuarioPadreFamilia/', include('usuarioPadreFamilia.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
