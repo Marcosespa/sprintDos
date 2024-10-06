92ff5d096d9a42f5dcecbf558c30e4cc26a5bc2a
Marcosespa <marcosespa24@gmail.com>
2024-10-06 07:15:11 -0500

    d

--- sprintDos/views.py
+++ sprintDos/views.py
@@ -14,6 +14,7 @@ def index(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
+        print(f"Usuario {username} ha iniciado sesión correctamente.")  # Mensaje en la consola
 
         if user is not None:
             login(request, user)
