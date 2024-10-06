780d088235bed60d26a389248392819e4365d8cb
iStork1 <f.jaimes@uniandes.edu.co>
2024-10-06 14:05:53 -0500

    .

--- usuarioPadreFamilia/models.py
+++ usuarioPadreFamilia/models.py
@@ -5,11 +5,9 @@ from cronograma.models import Cronograma
 # Create your models here.
 class UsuarioPadreFamilia(models.Model):
     username = models.CharField(max_length=50)
-    username = models.CharField(max_length=50)
     contrasenia = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=255, default='Sin descripción')
     estudiante_relacionado = models.CharField(max_length=100, default='No reconocido')
     
     def __str__(self):
-        return self.username
         return self.username
