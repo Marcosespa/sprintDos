464a3c27704d0d51193fa1dbe4ba505def4e9958
iStork1 <f.jaimes@uniandes.edu.co>
2024-10-09 10:42:58 -0500

    .

--- cronograma/models.py
+++ cronograma/models.py
@@ -8,7 +8,7 @@ class Cronograma(models.Model):
     mes = models.CharField(max_length=20)
     valor_total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     estado_pago = models.CharField(max_length=20, default='PENDIENTE')
-    usuario_padre = models.ForeignKey(UsuarioPadreFamilia, on_delete=models.CASCADE)
+    usuario_padre = models.ForeignKey(UsuarioPadreFamilia, on_delete=models.CASCADE, default=1)  # Cambia 1 por un ID válido
     
     def __str__(self):
         return f"{self.mes} - {self.estado_pago}"
