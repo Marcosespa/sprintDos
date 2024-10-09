68a608829126a77e687e06cd159bbe74e05466c6
Marcosespa <marcosespa24@gmail.com>
2024-10-08 21:09:54 -0500

    d

--- cronograma/urls.py
+++ cronograma/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.cronograma_index, name='cronograma_index'),
+    #path('', views.cronograma_index, name='cronograma_index'),
     path('cronograma_list/', views.cronograma_list, name='cronograma_list'),
 
 ]
