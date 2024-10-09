8f9059ce7f2603326ea8d8750a6760e6e914713b
Marcosespa <marcosespa24@gmail.com>
2024-10-08 19:26:09 -0500

    d

--- usuarioPadreFamilia/models.py
+++ usuarioPadreFamilia/models.py
@@ -1,34 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-class UsuarioPadreFamiliaManager(BaseUserManager):
-    def create_user(self, username, password=None, **extra_fields):
-        if not username:
-            raise ValueError('El nombre de usuario debe ser obligatorio')
-        user = self.model(username=username, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+# class UsuarioPadreFamiliaManager(BaseUserManager):
+#     def create_user(self, username, password=None, **extra_fields):
+#         if not username:
+#             raise ValueError('El nombre de usuario debe ser obligatorio')
+#         user = self.model(username=username, **extra_fields)
+#         user.set_password(password)
+#         user.save(using=self._db)
+#         return user
 
-    def create_superuser(self, username, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
+#     def create_superuser(self, username, password=None, **extra_fields):
+#         extra_fields.setdefault('is_staff', True)
+#         extra_fields.setdefault('is_superuser', True)
 
-        return self.create_user(username, password, **extra_fields)
+#         return self.create_user(username, password, **extra_fields)
 
-class UsuarioPadreFamilia(AbstractBaseUser, PermissionsMixin):
-    username = models.CharField(max_length=50, unique=True)
-    descripcion = models.CharField(max_length=255, default='Sin descripción')
-    password = models.CharField(max_length=128, default='default_password')  # Agrega un valor por defecto
-    estudiante_relacionado = models.CharField(max_length=100, default='No reconocido')
+# class UsuarioPadreFamilia(AbstractBaseUser, PermissionsMixin):
+#     username = models.CharField(max_length=50, unique=True)
+#     descripcion = models.CharField(max_length=255, default='Sin descripción')
+#     password = models.CharField(max_length=128, default='default_password')  # Agrega un valor por defecto
+#     estudiante_relacionado = models.CharField(max_length=100, default='No reconocido')
 
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
+#     is_active = models.BooleanField(default=True)
+#     is_staff = models.BooleanField(default=False)
 
-    objects = UsuarioPadreFamiliaManager()
+#     objects = UsuarioPadreFamiliaManager()
 
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = []
+#     USERNAME_FIELD = 'username'
+#     REQUIRED_FIELDS = []
 
-    def __str__(self):
-        return self.username
+#     def __str__(self):
+#         return self.username
