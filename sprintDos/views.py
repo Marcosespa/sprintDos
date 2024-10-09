0ffd3c6139f20f2711ce6dabcc88b6b78ab2ef2c
Marcosespa <marcosespa24@gmail.com>
2024-10-08 19:48:57 -0500

    d

--- sprintDos/views.py
+++ sprintDos/views.py
@@ -36,18 +36,28 @@ def crear_usuario(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
 
-        if UsuarioPadreFamilia.objects.filter(username=username).exists():
+        # Verificar si el nombre de usuario ya existe en alguna de las dos tablas
+        if UsuarioPadreFamilia.objects.filter(username=username).exists() or User.objects.filter(username=username).exists():
             messages.error(request, 'El nombre de usuario ya existe.')
         else:
             try:
-                # Crear un superusuario
-                usuario = UsuarioPadreFamilia(username=username)
-                usuario.set_password(password)  # Establecer la contraseña de manera segura
-                usuario.is_superuser = True  # Hacer al usuario un superusuario
-                usuario.is_staff = True  # Hacer que el usuario sea parte del staff para acceder al admin
-                usuario.is_active = True  # Asegurarse de que el usuario esté activo
-                usuario.save()
-                messages.success(request, 'Superusuario creado con éxito.')
+                # Crear usuario en UsuarioPadreFamilia
+                usuario_padre = UsuarioPadreFamilia(username=username)
+                usuario_padre.set_password(password)  # Establecer la contraseña de manera segura
+                usuario_padre.is_superuser = True  # Hacer al usuario un superusuario
+                usuario_padre.is_staff = True  # Permitir acceso a la administración
+                usuario_padre.is_active = True  # Asegurar que el usuario esté activo
+                usuario_padre.save()
+
+                # Crear usuario también en la tabla auth.User
+                user = User(username=username)
+                user.set_password(password)  # Establecer la contraseña
+                user.is_superuser = True  # Hacer al usuario superusuario en auth.User
+                user.is_staff = True  # Permitir acceso al admin
+                user.is_active = True  # Asegurar que el usuario esté activo
+                user.save()
+
+                messages.success(request, 'Usuario creado con éxito en ambas tablas.')
                 return redirect('index')
             except Exception as e:
                 messages.error(request, f'Error al crear el usuario: {str(e)}')
