5644dffa527457e64df5663f6ee157a5007f86c6
Valentina Ceron <l.ceronp@uniandes.edu.co>
2024-10-17 09:07:33 -0500

    'x'

--- pago/urls.py
+++ pago/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('procesar_pago/', procesar_pago, name='procesar_pago'), 
     path('editar_usuario/', usuario_padre_familia_edit_view, name='editar_usuario'),
     path('health-check/', views.healthCheck, name='health_check'),
-    path('crear-usuario/', crear_usuario, name='crear_usuario'),
+    #path('crear-usuario/', crear_usuario, name='crear_usuario'),
 ]
