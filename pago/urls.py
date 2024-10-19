3922b0a1fec2f27b1504f242dbd2f63c248bc2bf
Daniel Roa <d.roau@uniandes.edu.co>
2024-10-18 21:43:01 -0500

    commit Pago

--- pago/urls.py
+++ pago/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+
+from usuarioPadreFamilia.views import index_PadreFamilia
 from . import views
 from .views import crear_usuario, pagina_principal, procesar_pago, usuario_padre_familia_edit_view  
 
@@ -10,4 +12,5 @@ urlpatterns = [
     path('editar_usuario/', usuario_padre_familia_edit_view, name='editar_usuario'),
     path('health-check/', views.healthCheck, name='health_check'),
     #path('crear-usuario/', crear_usuario, name='crear_usuario'),
+    path('index_PadreFamilia/', index_PadreFamilia, name='index_PadreFamilia'),
 ]
--- pago/views.py
+++ pago/views.py
@@ -1,38 +1,30 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import Pago
-from django.contrib import messages 
-from django.contrib.auth.models import User
+from usuarioPadreFamilia.models import UsuarioPadreFamilia  # Asegúrate que esté bien importado
 
 @login_required
 def procesar_pago(request):
     if request.method == 'POST':
+        nombre_pago = request.POST.get('nombre_pago')
         valor_pago = request.POST.get('valor_pago')
+        fecha_pago = request.POST.get('fecha_pago')
         tipo_pago = request.POST.get('tipo_pago')
-        
-        # Validar que los campos no estén vacíos
-        if not valor_pago or not tipo_pago:
-            messages.error(request, 'Por favor, completa todos los campos.')
-            return redirect('procesar_pago')
-
-        try:
-            valor_pago = float(valor_pago) 
-        except ValueError:
-            messages.error(request, 'El valor del pago debe ser un número válido.')
-            return redirect('procesar_pago')
 
         # Crear el nuevo pago
         nuevo_pago = Pago(
-            valor_pago=valor_pago, 
-            tipo_pago=tipo_pago, 
+            nombre_pago=nombre_pago,
+            valor_pago=valor_pago,
+            fecha_pago=fecha_pago,
+            tipo_pago=tipo_pago,
             estado_pago='PENDIENTE',
-            usuario_padre=request.user  # Asociar el pago con el usuario actual
         )
         nuevo_pago.save()
-        
+
         messages.success(request, 'Pago procesado exitosamente.')
-        return redirect('usuarioPadreFamilia/index_PadreFamilia')
-    
+        return redirect('index_PadreFamilia')
+
     return render(request, 'procesar_pago.html')
+
