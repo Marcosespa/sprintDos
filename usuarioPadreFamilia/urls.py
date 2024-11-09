927bfd07a85f4a03158f173d802a454a4c45206e
Marcosespa <marcosespa24@gmail.com>
2024-11-08 21:00:21 -0500

    d

--- usuarioPadreFamilia/urls.py
+++ usuarioPadreFamilia/urls.py
@@ -5,11 +5,11 @@ from .views import index_PadreFamilia  # Importar la vista desde views.py
 
 urlpatterns = [
     path('index_PadreFamilia/', index_PadreFamilia, name='index_PadreFamilia'),  # Ruta dentro de 'usuarioPadreFamilia'
-#    path('salir/', views.salir, name="salir"),
+   path('salir/', views.salir, name="salir"),
     path('cronograma_index/', views.cronograma, name='cronograma_index'),
     path('health/', views.health_check, name='health'),
- #   path('procesar_pago/', views.pago, name='procesar_pago'),
-  #  path('pagos/', views.pagos_filtrados, name='pagos_filtrados'),
+    path('procesar_pago/', views.pago, name='procesar_pago'),
+    path('pagos/', views.pagos_filtrados, name='pagos_filtrados'),
 
 ]
 
