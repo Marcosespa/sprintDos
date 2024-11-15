9a60eac924bd8852341de5b62d70a434ad146a26
Daniel Roa <d.roau@uniandes.edu.co>
2024-11-15 00:08:29 -0500

    d

--- pago/forms.py
+++ pago/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Pago
+from django.utils import timezone
 
 class PagoForm(forms.ModelForm):
     class Meta:
@@ -11,3 +12,15 @@ class PagoForm(forms.ModelForm):
         if valor <= 0:
             raise forms.ValidationError('El valor del pago debe ser mayor a 0')
         return valor 
+
+    def clean_fecha_pago(self):
+        fecha_pago = self.cleaned_data['fecha_pago']
+        today = timezone.now().date()
+        if fecha_pago > today:
+            raise forms.ValidationError('La fecha de pago no puede ser una fecha futura')
+        return fecha_pago
+
+    # Optional: Add custom widgets or styles if needed
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.fields['nombre_pago'].widget.attrs.update({'class': 'custom-class'})
