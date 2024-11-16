4bab90aab72fff269b67415a0458b08f625051ea
iStork1 <f.jaimes@uniandes.edu.co>
2024-11-15 23:09:25 -0500

    Cambios pago , pruebas

--- pago/urls.py
+++ pago/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('procesar_pago/', views.procesar_pago, name='procesar_pago'),
+    path('procesar_pago/', views.procesar_pago, name='pago_procesar_pago'),
     path('gerente/pagosPendientes/', views.pago_pendiente, name='pago_pendiente'),
 ]
