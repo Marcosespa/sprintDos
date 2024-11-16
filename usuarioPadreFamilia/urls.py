4e07a82e5a04580a7e3a5b4b928e659b4739b4c9
iStork1 <f.jaimes@uniandes.edu.co>
2024-11-16 00:05:49 -0500

    .

--- usuarioPadreFamilia/urls.py
+++ usuarioPadreFamilia/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('cronograma_index/', views.cronograma, name='cronograma_index'),
     path('health/', views.health_check, name='health'),
     path('procesar_pago/', views.pago, name='procesar_pago'),
+    path('realizar_pago/', views.realizarpago, name='realizar_pago'),
     path('pagos/', views.pagos_filtrados, name='pagos_filtrados'),
     path('salir/', views.salir, name='salir')
 ]
