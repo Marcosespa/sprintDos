a67e3b824f9c797a60b1878dc506d2a1a9bc9959
Marcosespa <marcosespa24@gmail.com>
2024-11-15 21:45:07 -0500

    DD

--- sprintDos/auth0backend.py
+++ sprintDos/auth0backend.py
@@ -34,7 +34,7 @@ class Auth0(BaseOAuth2):
             "picture": userinfo["picture"],
             "user_id": userinfo["sub"]
         }
-
+#COMENTARIO PARA MODIIFCAR
 # Esta función está POR FUERA de la clase Auth0. Es una función independiente.
 def getRole(request):
     user = request.user
